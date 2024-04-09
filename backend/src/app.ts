24e555d1eab908352e517757eb51ad0943fa5b9b
Aditya Dhage <adhage93@gmail.com>
2024-04-08 20:46:49 -0700

    Get Current Track Issue

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,28 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 import { Logger } from "./utils/logger/logger.js";
 import { ExternalApiCall } from "./external/external-api-call.js";
 import { ExternalApiCallError } from "./types/errors.js";
+import { SpotifySDK } from "./external/spotify-sdk.js";
 
 dotenv.config();
 
 const app: Express = express();
+app.use(cors());
+app.use(express.static(path.resolve(__dirname, "..", "public")));
+
 const port = parseInt(process.env.SONG_POOL_SERVER_PORT!) || 3000;
 
+let currentSpotifyWebSDK: SpotifySDK;
+
 app.get("/", (req: Request, res: Response) => {
-    res.send("root");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/token", (req: Request, res: Response) => {
@@ -38,6 +49,17 @@ app.get("/track_info", (req: Request, res: Response) => {
         .catch((error: ExternalApiCallError) => {
             res.status(500).send(error.fetchCallResponse);
         });
+});
+
+// Endpoints for Server Side Spotify User
+
+app.get("/server-user-login", (req: Request, res: Response) => {
+    Logger.info("GET - /server-user-login");
+    let webApi = SpotifySDK.getUserAuthorization();
+});
+
+app.post("/server-user-login/callback", (req: Request, res: Response) => {
+    Logger.info(JSON.stringify(req));
 });
 
 app.listen(port, () => {
---
+++ backend/src/external/spotify-sdk.ts
@@ -0,0 +1,94 @@
+import {
+    SpotifyApi,
+    RecommendationsRequest,
+    Track,
+    Market,
+    ClientCredentialsStrategy,
+    AuthorizationCodeWithPKCEStrategy,
+} from "@spotify/web-api-ts-sdk";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { Logger } from "../utils/logger/logger.js";
+
+export class SpotifySDK {
+    private static instance: SpotifySDK | null = null;
+    private currentWebAPI: SpotifyApi;
+
+    private static spotifyMarket: string = process.env.SONG_MARKET!;
+    private static songRecommendations: number = parseInt(process.env.SONG_RECOMMENDATIONS!);
+    private static spotifyClientId: string = process.env.SPOTIFY_CLIENT_ID!;
+    private static scopes: string[] = process.env.SPOTIFY_AUTH_SCOPE!.split(",");
+
+    private constructor(webApi: SpotifyApi) {
+        this.currentWebAPI = webApi;
+    }
+
+    public static getInstance(webApi: SpotifyApi): SpotifySDK {
+        if (SpotifySDK.instance) {
+            return SpotifySDK.instance;
+        } else {
+            return new SpotifySDK(webApi);
+        }
+    }
+
+    // private initializeSDK(): SpotifyApi {
+    //     const spotifyClientSecrete: string = process.env.SPOTIFY_CLIENT_SECRET!;
+    //     let sdk: SpotifyApi;
+    //     try {
+    //         let clientCredential = new ClientCredentialsStrategy(
+    //             SpotifySDK.spotifyClientId,
+    //             spotifyClientSecrete,
+    //             SpotifySDK.scopes
+    //         );
+    //         sdk = new SpotifyApi(clientCredential);
+    //     } catch (error) {
+    //         Logger.error("Exception while creating Spotify SDK Auth instance", error);
+    //         throw error;
+    //     }
+    //     return sdk;
+    // }
+
+    public static getUserAuthorization() {
+        let sdk: SpotifyApi;
+        try {
+            sdk = SpotifyApi.withUserAuthorization(
+                SpotifySDK.spotifyClientId,
+                "http://localhost:3456/server-user-login/callback",
+                SpotifySDK.scopes
+            );
+        } catch (error) {
+            Logger.error("Exception while getting user Authorization", error);
+            throw error;
+        }
+        return sdk;
+    }
+
+    async getTrackRecommendations(seed_track_id: string): Promise<Track[]> {
+        let request: RecommendationsRequest = {
+            limit: SpotifySDK.songRecommendations,
+            market: SpotifySDK.spotifyMarket,
+            seed_tracks: [seed_track_id],
+        };
+        Logger.debug(JSON.stringify(request));
+        try {
+            let apiResponse = await this.currentWebAPI.recommendations.get(request);
+            return apiResponse.tracks;
+        } catch (error) {
+            Logger.error("Exception while getting track recommendations from spotify web api", error);
+            throw error;
+        }
+    }
+
+    async getCurrentlyPlayingTrack(): Promise<Track> {
+        try {
+            let apiResponse = await this.currentWebAPI.player.getCurrentlyPlayingTrack(
+                SpotifySDK.spotifyMarket as Market
+            );
+            return apiResponse.item as Track;
+        } catch (error) {
+            Logger.error("Exception while getting track recommendations from spotify web api", error);
+            throw error;
+        }
+    }
+}
